4fca2f0a1df0f06b91b5f6decc20c545eb7173eb
unknown <ardiansyah7b7@gmail.com>
2023-12-04 14:58:53 +0700

    from ardi

--- page.hpp
+++ page.hpp
@@ -10,3 +10,14 @@ void open_page(){
 	cout << "2. History" << endl;
 	
 }
+
+void page_translate(){
+    cout << "======================================" << endl;
+	cout << "|         SELAMAT DATANG DI          |" << endl;
+	cout << "|            MORSELATOR              |" << endl;
+	cout << "======================================" << endl;
+    cout << "" << endl;
+    cout << "        Pilih Mode Translate           " << endl;
+    cout << "         1. Teks  ---> Morse          " << endl;
+    cout << "         2. Morse ---> Teks           " << endl;
+}
