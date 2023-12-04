8c491c176ae0c1d595cce54984e7070421ba0d69
unknown <ardiansyah7b7@gmail.com>
2023-12-04 14:55:47 +0700

    from ardi

--- page.hpp
+++ page.hpp
@@ -3,6 +3,10 @@ using namespace std;
 
 void open_page(){
 	cout << "===============================" << endl;
-	cout << "|            Halo             |" << endl;
+	cout << "|         MORSELATOR          |" << endl;
 	cout << "===============================" << endl;
+	cout << "" << endl;
+	cout << "1. Translate" << endl;
+	cout << "2. History" << endl;
+	
 }
