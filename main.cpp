a869b50914ff13b83094e539c191c52b8ff23d53
unknown <ardiansyah7b7@gmail.com>
2023-12-09 21:20:10 +0700

    from ardi

--- main.cpp
+++ main.cpp
@@ -8,6 +8,28 @@ int main(){
 
     open_page();
 
+int printascii(string filename)
+{
+	string line = "";
+	ifstream infile;
+	infile.open("art_ascii.txt");
+	if(infile.is_open())
+	{
+		while(getline(infile,line))
+		{
+			cout << line << endl;
+		}
+	}
+	else
+	{
+		cout << "File Failed To Load." << endl;
+	}
+	infile.close();
+
+
+
+}
+
     return 0;
     
 }
